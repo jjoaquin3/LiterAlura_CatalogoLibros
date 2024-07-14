5199eaa6da151dd29bcecfda36ee18e56b798abe
Jose Chub <jjcacao2@gmail.com>
2024-07-13 22:20:54 -0600

    Configuración Docker para Base de Datos, con Contenedor final llamado LITERALURA_DB

---
+++ docker-db/initdb/create_schema.sql
@@ -0,0 +1,5 @@
+-- Crear el esquema LITERALURA
+CREATE SCHEMA "LITERALURA";
+
+-- Configurar la diferencia entre mayúsculas y minúsculas
+ALTER DATABASE "ALURA_TEST" SET standard_conforming_strings = 'on';
